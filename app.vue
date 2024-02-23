0e5a00e414eb20ecc237a3009009040a2fbad1e4
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-02-23 13:43:52 +0900

    feat: cats are on metro stations

--- app.vue
+++ app.vue
@@ -3,18 +3,45 @@
 </template>
 
 <script setup lang="ts">
+import mapboxgl from "mapbox-gl";
 
-import mapboxgl from 'mapbox-gl';
-
-const config = useRuntimeConfig()
+const config = useRuntimeConfig();
 
 onMounted(() => {
-  mapboxgl.accessToken = config.public.mapboxAccessToken
-  new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  mapboxgl.accessToken = config.public.mapboxAccessToken;
+  const map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/mapbox/standard", // style URL
     center: [121.497366, 25.09497], // starting position [lng, lat]
     zoom: 11, // starting zoom
   });
-})
+
+  map.on("load", () => {
+    map.loadImage(
+      "https://docs.mapbox.com/mapbox-gl-js/assets/cat.png",
+      (error, image) => {
+        if (error) throw error;
+
+        // Add the image to the map style.
+        map.addImage("cat", image);
+
+        map.addSource("taipeiMetro", {
+          type: "geojson",
+          data: "/taipei-metro.geojson",
+        });
+
+        map.addLayer({
+          source: "taipeiMetro",
+          id: "stations",
+          type: "symbol",
+          layout: {
+            "icon-image": "cat", // reference the image
+            "icon-size": 0.1,
+            "icon-allow-overlap": true
+          },
+        });
+      }
+    );
+  });
+});
 </script>
