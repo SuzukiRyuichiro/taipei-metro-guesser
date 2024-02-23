f9fbcbe5b9de90d350ddb95eeb64dcbb51344e3b
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-02-23 10:45:28 +0900

    added map

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div id="map" class="h-screen"></div>
 </template>
+
+<script setup lang="ts">
+
+import mapboxgl from 'mapbox-gl';
+
+const config = useRuntimeConfig()
+
+onMounted(() => {
+  mapboxgl.accessToken = config.public.mapboxAccessToken
+  new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [121.497366, 25.09497], // starting position [lng, lat]
+    zoom: 11, // starting zoom
+  });
+})
+</script>
