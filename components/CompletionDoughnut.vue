54a780dc9cfaeb2b5a191f33661c07cc23784587
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-02-25 00:41:39 +0900

    test

--- components/CompletionDoughnut.vue
+++ components/CompletionDoughnut.vue
@@ -16,41 +16,41 @@ const props = defineProps<{
 }>();
 
 ChartJS.register(ArcElement);
-ChartJS.register({
-  id: "drawInside",
-  beforeDatasetDraw(chart, args, pluginOptions) {
-    const { ctx, data, config, chartArea } = chart;
-    ctx.save();
-    const baseSize = Math.min(
-      chartArea.right - chartArea.left,
-      chartArea.bottom - chartArea.top
-    );
-    const fontSize = Math.max(12, baseSize / 4); // Adjust the divisor to control the scale
+// ChartJS.register({
+//   id: "drawInside",
+//   beforeDatasetDraw(chart, args, pluginOptions) {
+//     const { ctx, data, config, chartArea } = chart;
+//     ctx.save();
+//     const baseSize = Math.min(
+//       chartArea.right - chartArea.left,
+//       chartArea.bottom - chartArea.top
+//     );
+//     const fontSize = Math.max(12, baseSize / 4); // Adjust the divisor to control the scale
 
-    // Apply the responsive font size
-    ctx.font = `bold ${fontSize}px sans-serif`;
+//     // Apply the responsive font size
+//     ctx.font = `bold ${fontSize}px sans-serif`;
 
-    // draw circle
-    const centerX = (chartArea.left + chartArea.right) / 2;
-    const centerY = (chartArea.top + chartArea.bottom) / 2;
+//     // draw circle
+//     const centerX = (chartArea.left + chartArea.right) / 2;
+//     const centerY = (chartArea.top + chartArea.bottom) / 2;
 
-    // Draw the background circle
-    const innerRadius = chart.getDatasetMeta(0).data[0].innerRadius;
-    const bgCircleColor = chart.getDatasetMeta(0)._dataset.backgroundColor[1]; // Color of the filled circle background
-    ctx.fillStyle = bgCircleColor;
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, innerRadius, 0, 2 * Math.PI);
-    ctx.fill();
+//     // Draw the background circle
+//     const innerRadius = chart.getDatasetMeta(0).data[0].innerRadius;
+//     const bgCircleColor = chart.getDatasetMeta(0)._dataset.backgroundColor[1]; // Color of the filled circle background
+//     ctx.fillStyle = bgCircleColor;
+//     ctx.beginPath();
+//     ctx.arc(centerX, centerY, innerRadius, 0, 2 * Math.PI);
+//     ctx.fill();
 
-    const xCoordinate = chart.getDatasetMeta(0).data[0].x;
-    const yCoordinate = chart.getDatasetMeta(0).data[0].y;
-    const { lineAcronym } = data.datasets[0];
-    ctx.fillStyle = lineAcronym === "O" ? "#110905" : "#fff";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText(lineAcronym, xCoordinate, yCoordinate);
-  },
-});
+//     const xCoordinate = chart.getDatasetMeta(0).data[0].x;
+//     const yCoordinate = chart.getDatasetMeta(0).data[0].y;
+//     const { lineAcronym } = data.datasets[0];
+//     ctx.fillStyle = lineAcronym === "O" ? "#110905" : "#fff";
+//     ctx.textAlign = "center";
+//     ctx.textBaseline = "middle";
+//     ctx.fillText(lineAcronym, xCoordinate, yCoordinate);
+//   },
+// });
 
 const chartData = computed(() => ({
   datasets: [
